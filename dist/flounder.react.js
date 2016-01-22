837116deb3bdd0be59133437cb284764229921fd
mouse <mouse@sociomantic.com>
2016-01-22 12:22:55 +0100

    modified data type detection
    closes #30

--- dist/flounder.react.js
+++ dist/flounder.react.js
@@ -6,7 +6,7 @@
  * Released under the MIT license
  * https://github.com/sociomantic/flounder/license
  *
- * Date: Thu Jan 21 2016
+ * Date: Fri Jan 22 2016
  * "This, so far, is the best Flounder ever"
  */
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
@@ -20960,7 +20960,9 @@ var build = {
         };
 
         originalData.forEach(function (dataObj, i) {
-            if (typeof dataObj === 'string') {
+            var dataObjType = typeof dataObj;
+
+            if (dataObjType !== 'object') {
                 dataObj = originalData[i] = {
                     text: dataObj,
                     value: dataObj
