52bbf94b6f3bb8d05d3556e08e9b797a6569889f
Mouse Braun <mouse@knoblau.ch>
2016-02-26 12:01:58 +0100

    updated http test

--- tests/unit/utilsTest.js
+++ tests/unit/utilsTest.js
@@ -181,6 +181,40 @@ let tests = function( Flounder )
     QUnit.test( 'http', function( assert )
     {
         assert.ok( utils.http, 'exists' );
+
+        var getTest      = assert.async();
+        utils.http( { url: './httpTest.html', method: 'GET' } ).then( function( data )
+        {
+            assert.equal( data, 'moon', 'page correctly retrieved' );
+            getTest();
+        } );
+
+        var parameterTest      = assert.async();
+        utils.http( {
+                    url         : './httpTest.html',
+                    method      : 'GET',
+                    headers     : {
+                        Accept      : 'text/plain'
+                    },
+                    async       : true
+                }
+        ).then( function( data )
+        {
+            assert.equal( data, 'moon', 'parameters are recieved correctly' );
+            parameterTest();
+        } );
+
+        var errorTest      = assert.async();
+        utils.http( { url : './httpTest.hml' }
+        ).catch( function( e )
+        {
+            e = ( e instanceof Error );
+            assert.equal( e, true, 'errors are handled correctly' );
+            errorTest();
+        } );
+
+        buildTest( 'Speed depends on network traffic.' );
+    });
     } );
 
 
