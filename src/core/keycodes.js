f478c0fc51618f419d83620a39812188c117589b
Mahmoud Zakria <mahmoud.zakria@sociomantic.com>
2018-09-03 12:53:10 +0200

    added copyright statements

--- src/core/keycodes.js
+++ src/core/keycodes.js
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2016 dunnhumby Germany GmbH.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the LICENSE file
+ * in the root directory of this source tree.
+ *
+ */
+
 const keycodes = {
     BACKSPACE           : 8,
     DOWN                : 40,
--- src/wrappers/flounder.amd.js
+++ src/wrappers/flounder.amd.js
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2016 dunnhumby Germany GmbH.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the LICENSE file
+ * in the root directory of this source tree.
+ *
+ */
+
 /* global define */
 import Flounder from '../core/flounder';
 
